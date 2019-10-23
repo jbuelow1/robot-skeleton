a88fd8110e13869304a4e85ebae47dc679853014
jdbuelow1 <jdbuelow@gmail.com>
2019-10-23 11:30:17 -0500

    servo frequency is now set

--- src/main/java/com/jbuelow/robotskeleton/service/flow/MainFlowService.java
+++ src/main/java/com/jbuelow/robotskeleton/service/flow/MainFlowService.java
@@ -3,6 +3,8 @@ package com.jbuelow.robotskeleton.service.flow;
 import com.github.sarxos.webcam.WebcamDevice;
 import com.jbuelow.robotskeleton.hardware.camera.CameraDevice;
 import com.jbuelow.robotskeleton.hardware.camera.impl.WebcamCaptureDevice;
+import com.jbuelow.robotskeleton.hardware.motor.servo.PwmDevice;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +15,14 @@ public class MainFlowService {
   private boolean running = false;
 
   private final CameraDevice webcam;
+  private final PwmDevice pwmDevice;
 
-  public MainFlowService(CameraDevice webcam) {
+  public MainFlowService(CameraDevice webcam, PwmDevice pwmDevice) throws IOException {
     log.debug("Initializing flow service");
     this.webcam = webcam;
+    this.pwmDevice = pwmDevice;
+
+    pwmDevice.setPWMFreqency(1000);
   }
 
 }
