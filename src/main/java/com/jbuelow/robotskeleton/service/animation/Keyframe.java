b1fde42029326bbe946d325ff45fbecd88b5da02
jdbuelow1 <jdbuelow@gmail.com>
2019-10-27 20:40:53 -0500

    populated the keyframe class for later

--- src/main/java/com/jbuelow/robotskeleton/service/animation/Keyframe.java
+++ src/main/java/com/jbuelow/robotskeleton/service/animation/Keyframe.java
@@ -1,5 +1,13 @@
 package com.jbuelow.robotskeleton.service.animation;
 
-public class Keyframe {
+import java.util.Map;
+
+public abstract class Keyframe {
+
+  public abstract Map<Integer, Integer> getPositions();
+
+  public Integer getPosition(Integer channel) {
+    return this.getPositions().get(channel);
+  }
 
 }
